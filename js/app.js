81bde4c8e4aa23ad10091d7905c375ec33dbc90b
Luz Gutierrez <mili01gm@gmail.com>
2017-04-19 13:25:22 -0500

    Avance cancha grande

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var cancha = document.createElement("div");
+cancha.setAttribute("id","cancha");
+var innerCancha = document.createElement("div");
+innerCancha.setAttribute("id","inner-cancha");
+
+
+
+
+
+
+
+
+
+
+cancha.appendChild(innerCancha);
+
+document.getElementById('estadio').appendChild(cancha);

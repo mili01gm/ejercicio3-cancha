9612541c8b5556912b33e78a0da2c0b789380002
Luz Gutierrez <mili01gm@gmail.com>
2017-04-19 13:28:11 -0500

    hasta los arcos

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ var cancha = document.createElement("div");
 cancha.setAttribute("id","cancha");
 var innerCancha = document.createElement("div");
 innerCancha.setAttribute("id","inner-cancha");
+var arco1 = document.createElement("div");
+var arco2 = document.createElement("div");
+arco1.setAttribute("id","arco1");
+arco2.setAttribute("id","arco2");
 
 
 
@@ -10,8 +14,8 @@ innerCancha.setAttribute("id","inner-cancha");
 
 
 
-
-
+innerCancha.appendChild(arco1);
+innerCancha.appendChild(arco2);
 cancha.appendChild(innerCancha);
 
 document.getElementById('estadio').appendChild(cancha);
